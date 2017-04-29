8aa039623c23c2c78cb6a2c30042a58db89a8195
CrazyApeDX <dxcdkey631231@sina.com>
2017-04-29 15:39:28 +0800

    初始化包结构

---
+++ lyz-app-management/src/main/java/cn/com/leyizhuang/app/ApplicationStarter.java
@@ -0,0 +1,15 @@
+package cn.com.leyizhuang.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author CrazyApeDX
+ *         Created on 2017/4/29.
+ */
+@SpringBootApplication
+public class ApplicationStarter {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationStarter.class, args);
+    }
+}
