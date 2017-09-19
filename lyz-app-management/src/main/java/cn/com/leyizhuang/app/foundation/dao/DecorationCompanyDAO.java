39cef77829fb16b4a3ae133fe9e523949fdae73f
dailu12 <Dai1097114100>
2017-09-19 16:53:20 +0800

    装饰公司

---
+++ lyz-app-management/src/main/java/cn/com/leyizhuang/app/foundation/dao/DecorationCompanyDAO.java
@@ -0,0 +1,13 @@
+package cn.com.leyizhuang.app.foundation.dao;
+
+import cn.com.leyizhuang.app.foundation.pojo.DecorationCompanyDO;
+import cn.com.leyizhuang.common.foundation.dao.BaseDAO;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author GenerationRoad
+ * @date 2017/9/19
+ */
+@Repository
+public interface DecorationCompanyDAO extends BaseDAO<DecorationCompanyDO>{
+}
