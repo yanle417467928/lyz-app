5b5d00d0a7226a98a1d8d3de87f6ba44b2b2a8a7
dailu12 <Dai1097114100>
2017-09-20 16:56:36 +0800

    装饰公司员工

---
+++ lyz-app-management/src/main/java/cn/com/leyizhuang/app/foundation/pojo/dto/FitmentCompanyUserDTO.java
@@ -0,0 +1,40 @@
+package cn.com.leyizhuang.app.foundation.pojo.dto;
+
+import cn.com.leyizhuang.app.core.constant.SexType;
+
+import java.io.Serializable;
+
+/**
+ * @author GenerationRoad
+ * @date 2017/9/20
+ */
+public class FitmentCompanyUserDTO implements Serializable {
+    private static final long serialVersionUID = -5457859636157519043L;
+
+    // 自增主键
+    private Long id;
+
+    // 员工姓名
+    private String name;
+
+    // 员工手机号码
+    private String phone;
+
+    // 员工密码
+    private String password;
+
+    // 是否是主账号
+    private Boolean isMain = true;
+
+    // 装饰公司id
+    private Long companyId;
+
+    // 是否被冻结，默认未冻结
+    private Boolean frozen = false;
+
+    //性别
+    private SexType sex;
+
+    //年龄
+    private Integer age;
+}
