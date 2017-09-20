c827a40a3ee6eebf5132eaa69facbb15e1e70385
leyan <417467928@qq.com>
2017-09-20 16:30:23 +0800

    去掉空引用

--- lyz-app-facade/src/main/java/cn/com/leyizhuang/app/foundation/service/Impl/AppUserService.java
+++ lyz-app-facade/src/main/java/cn/com/leyizhuang/app/foundation/service/Impl/AppUserService.java
@@ -6,8 +6,6 @@ import cn.com.leyizhuang.app.foundation.service.IAppUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.Resource;
-
 /**
  * lyz-app-facade用户服务实现类
  *
