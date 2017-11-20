9582eab3bfea4bd81f8dc4bbc2bc173678d1481f
dailu12 <Dai1097114100>
2017-11-20 15:41:02 +0800

    支付宝充值预存款

---
+++ lyz-app-pojo/src/main/java/cn/com/leyizhuang/app/core/constant/PaymentDataType.java
@@ -0,0 +1,21 @@
+package cn.com.leyizhuang.app.core.constant;
+
+/**
+ * @author GenerationRoad
+ * @date 2017/11/18
+ */
+public enum PaymentDataType {
+    ST_PRE_DEPOSIT("门店预存款充值"), CUS_PRE_DEPOSIT("客户预存款充值"),
+    DEC_PRE_DEPOSIT("装饰公司预存款充值"), ORDER("订单支付");
+
+
+    private final String value;
+
+    PaymentDataType(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
