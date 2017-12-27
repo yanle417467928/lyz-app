3341c82c0327feba256a9a8311a129a7d4370e14
leyan <417467928@qq.com>
2017-12-27 15:13:40 +0800

    引入spring cloud stream rabbitmq,搭建rabbitmq消息队列

---
+++ lyz-app-facade/src/test/java/cn/com/leyizhuang/app/LyzAppTests.java
@@ -0,0 +1,14 @@
+package cn.com.leyizhuang.app;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ApplicationStarter.class)
+@WebAppConfiguration
+public class LyzAppTests {
+
+
+}
