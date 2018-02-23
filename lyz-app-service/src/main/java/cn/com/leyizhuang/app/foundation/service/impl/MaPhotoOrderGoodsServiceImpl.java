e212869a3c14928af5aeffda52b66d8daa06aa47
dailu12 <Dai1097114100>
2018-02-23 15:52:56 +0800

    拍照下单记录选择商品信息

---
+++ lyz-app-service/src/main/java/cn/com/leyizhuang/app/foundation/service/impl/MaPhotoOrderGoodsServiceImpl.java
@@ -0,0 +1,27 @@
+package cn.com.leyizhuang.app.foundation.service.impl;
+
+import cn.com.leyizhuang.app.foundation.dao.MaPhotoOrderGoodsDAO;
+import cn.com.leyizhuang.app.foundation.pojo.PhotoOrderGoodsDO;
+import cn.com.leyizhuang.app.foundation.service.MaPhotoOrderGoodsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+/**
+ * @author GenerationRoad
+ * @date 2018/2/23
+ */
+@Service
+@Transactional
+public class MaPhotoOrderGoodsServiceImpl implements MaPhotoOrderGoodsService {
+
+    @Autowired
+    private MaPhotoOrderGoodsDAO maPhotoOrderGoodsDAO;
+
+    @Override
+    public int batchSave(List<PhotoOrderGoodsDO> photoOrderGoodsDOList) {
+        return this.maPhotoOrderGoodsDAO.batchSave(photoOrderGoodsDOList);
+    }
+}
