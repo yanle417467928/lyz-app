103d45768c0474a61a4271c12861de34251fb1e3
leyan <417467928@qq.com>
2018-03-14 11:45:33 +0800

    退单拆单refund_amount金额不对

--- lyz-app-service/src/main/java/cn/com/leyizhuang/app/foundation/service/impl/AppSeparateOrderServiceImpl.java
+++ lyz-app-service/src/main/java/cn/com/leyizhuang/app/foundation/service/impl/AppSeparateOrderServiceImpl.java
@@ -608,7 +608,7 @@ public class AppSeparateOrderServiceImpl implements AppSeparateOrderService {
                         returnOrderBaseInf.setMainReturnNumber(returnOrderBaseInfo.getReturnNo());
                         returnOrderBaseInf.setReturnNumber(OrderUtils.generateSeparateReturnOrderNumber(flag, returnOrderBaseInfo.getReturnNo()));
                         returnOrderBaseInf.setOrderTypeId(orderBaseInf.getOrderTypeId());
-                        returnOrderBaseInf.setRefundAmount(returnOrderBaseInfo.getReturnPrice());
+                        //returnOrderBaseInf.setRefundAmount(returnOrderBaseInfo.getReturnPrice());
                         returnOrderBaseInf.setReturnDate(returnOrderBaseInfo.getReturnTime());
                         returnOrderBaseInf.setReturnType(returnOrderBaseInfo.getReturnType());
                         returnOrderBaseInf.setRtFullFlag(returnOrderBaseInfo.getReturnType() == ReturnOrderType.NORMAL_RETURN ? AppWhetherFlag.N : AppWhetherFlag.Y);
@@ -623,6 +623,7 @@ public class AppSeparateOrderServiceImpl implements AppSeparateOrderService {
                         }
 
                         //创建分退单产品信息
+                        Double refundAmount = 0D;
                         List<ReturnOrderGoodsInf> returnOrderGoodsInfList = new ArrayList<>(20);
                         if (null != separateReturnOrderGoodsInfoList && separateReturnOrderGoodsInfoList.size() > 0) {
                             for (ReturnOrderGoodsInfo returnGoodsInfo : separateReturnOrderGoodsInfoList) {
@@ -645,6 +646,8 @@ public class AppSeparateOrderServiceImpl implements AppSeparateOrderService {
                                 returnOrderGoodsInfList.add(returnGoodsInf);
                             }
                         }
+                        refundAmount = returnOrderGoodsInfList.stream().mapToDouble(p -> p.getQuantity() * p.getReturnPrice()).sum();
+                        returnOrderBaseInf.setRefundAmount(refundAmount);
                         returnOrderParamMap.put(returnOrderBaseInf, returnOrderGoodsInfList);
                     } else {
                         throw new RuntimeException("未找到原分单信息,退单拆单失败!");
