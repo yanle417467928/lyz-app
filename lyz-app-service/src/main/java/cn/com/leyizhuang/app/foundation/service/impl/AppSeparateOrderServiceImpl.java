0709e6c0406165c07226c611d5a3fea42686d8aa
leyan <417467928@qq.com>
2018-03-15 13:55:43 +0800

    门店预存款提现没有发退款给EBS

--- lyz-app-service/src/main/java/cn/com/leyizhuang/app/foundation/service/impl/AppSeparateOrderServiceImpl.java
+++ lyz-app-service/src/main/java/cn/com/leyizhuang/app/foundation/service/impl/AppSeparateOrderServiceImpl.java
@@ -184,7 +184,7 @@ public class AppSeparateOrderServiceImpl implements AppSeparateOrderService {
                                 separateOrderMemberDiscount += (goodsInfo.getRetailPrice() - goodsInfo.getSettlementPrice()) * goodsInfo.getOrderQuantity();
                                 separateOrderPromotionDiscount += goodsInfo.getPromotionSharePrice() * goodsInfo.getOrderQuantity();
                                 separateOrderCashCouponDiscount += goodsInfo.getCashCouponSharePrice() * goodsInfo.getOrderQuantity();
-                               // separateOrderLebiDiscount += goodsInfo.getLbSharePrice() * goodsInfo.getOrderQuantity();
+                                // separateOrderLebiDiscount += goodsInfo.getLbSharePrice() * goodsInfo.getOrderQuantity();
                                 separateOrderSubventionDiscount += goodsInfo.getCashReturnSharePrice() * goodsInfo.getOrderQuantity();
                             }
 
@@ -969,13 +969,14 @@ public class AppSeparateOrderServiceImpl implements AppSeparateOrderService {
             //获取门店充值信息
             else if (withdrawRefundInfo.getWithdrawAccountType() == RechargeAccountType.ST_PREPAY) {
                 StPreDepositWithdraw stPreDepositWithdraw = stPreDepositWithdrawDAO.findByApplyNo(withdrawRefundInfo.getWithdrawNo());
-                AppStore store = storeService.findById(stPreDepositWithdraw.getApplyStId());
-                withdrawRefundInf.setDiySiteCode(store.getStoreCode());
-                withdrawRefundInf.setStoreOrgCode(store.getStoreStructureCode());
-                withdrawRefundInf.setSobId(store.getSobId());
+
+                AppStore store = storeService.findStoreByUserIdAndIdentityType(stPreDepositWithdraw.getApplyStId(), AppIdentityType.SELLER.getValue());
+                if (null != store) {
+                    withdrawRefundInf.setDiySiteCode(store.getStoreCode());
+                    withdrawRefundInf.setStoreOrgCode(store.getStoreStructureCode());
+                    withdrawRefundInf.setSobId(store.getSobId());
+                }
             }
-
-
             //保存提现退款接口信息
             separateOrderDAO.saveWithdrawRefundInf(withdrawRefundInf);
         }
