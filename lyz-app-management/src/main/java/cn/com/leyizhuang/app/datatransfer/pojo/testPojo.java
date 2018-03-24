8ccba9ea88603226d22493ced51a04d01d70a9ba
panjie <1742552645@qq.com>
2018-03-24 09:10:55 +0800

    数据迁移包创建

---
+++ lyz-app-management/src/main/java/cn/com/leyizhuang/app/datatransfer/pojo/testPojo.java
@@ -0,0 +1,7 @@
+package cn.com.leyizhuang.app.datatransfer.pojo;
+
+/**
+ * Created by panjie on 2018/3/24.
+ */
+public class testPojo {
+}
---
+++ lyz-app-management/src/main/java/cn/com/leyizhuang/app/datatransfer/service/testService.java
@@ -0,0 +1,7 @@
+package cn.com.leyizhuang.app.datatransfer.service;
+
+/**
+ * Created by panjie on 2018/3/24.
+ */
+public class testService {
+}
