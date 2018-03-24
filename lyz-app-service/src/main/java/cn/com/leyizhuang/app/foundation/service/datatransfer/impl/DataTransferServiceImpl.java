9408753b377aea4ec2e3a3e8e8bc455521838e4e
leyan <417467928@qq.com>
2018-03-24 16:12:02 +0800

    导数据初始化

--- lyz-app-service/src/main/java/cn/com/leyizhuang/app/foundation/service/datatransfer/impl/DataTransferServiceImpl.java
+++ lyz-app-service/src/main/java/cn/com/leyizhuang/app/foundation/service/datatransfer/impl/DataTransferServiceImpl.java
@@ -2,12 +2,15 @@ package cn.com.leyizhuang.app.foundation.service.datatransfer.impl;
 
 import cn.com.leyizhuang.app.foundation.pojo.datatransfer.TdOrderGoods;
 import cn.com.leyizhuang.app.foundation.pojo.order.OrderGoodsInfo;
+import cn.com.leyizhuang.app.foundation.service.datatransfer.DataTransferService;
 
 /**
  * 订单商品转换类
- * Created by panjie on 2018/3/24.
+ *
+ * @author Richard
+ * @date 2018/3/24
  */
-public class OrderGoodsInoTransferServiceImpl {
+public class DataTransferServiceImpl implements DataTransferService {
 
     public OrderGoodsInfo transferOne(TdOrderGoods tdOrderGoods){
         OrderGoodsInfo goodsInfo = new OrderGoodsInfo();
