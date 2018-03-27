e03b3ec98ed71250379df03d6a9801dd574e42cf
caiyu0608 <393655489@qq.com>
2018-03-27 11:23:17 +0800

    账单数据导入方法

--- lyz-app-service/src/main/java/cn/com/leyizhuang/app/foundation/service/datatransfer/DataTransferService.java
+++ lyz-app-service/src/main/java/cn/com/leyizhuang/app/foundation/service/datatransfer/DataTransferService.java
@@ -32,7 +32,7 @@ public interface DataTransferService {
 
     Integer transferCoupon(OrderBaseInfo baseInfo);
 
-    void transferOrderBillingDetails();
+    Integer transferOrderBillingDetails();
 
     AppEmployee findFitEmployeeInfoById(Long userId);
 
