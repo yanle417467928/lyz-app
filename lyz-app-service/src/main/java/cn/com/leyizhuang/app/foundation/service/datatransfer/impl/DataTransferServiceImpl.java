dbc761488f56c2e0aae9d3eb9d6689dd2dbee0b8
liuhao <540654127@qq.com>
2018-03-29 13:38:03 +0800

    物流信息导入

--- lyz-app-service/src/main/java/cn/com/leyizhuang/app/foundation/service/datatransfer/impl/DataTransferServiceImpl.java
+++ lyz-app-service/src/main/java/cn/com/leyizhuang/app/foundation/service/datatransfer/impl/DataTransferServiceImpl.java
@@ -106,7 +106,9 @@ public class DataTransferServiceImpl implements DataTransferService {
         OrderLogisticsInfo orderLogisticsInfo = new OrderLogisticsInfo();
 
         if ("门店自提".equals(tdOrder.getDeliverTypeTitle())) {
-            orderLogisticsInfo.setDetailedAddress(null);
+            orderLogisticsInfo.setReceiver(null);
+            orderLogisticsInfo.setReceiverPhone(null);
+            orderLogisticsInfo.setShippingAddress(null);
             List<AppStore> filterStoreList = storeList.stream().filter(p -> p.getStoreCode().equals(tdOrder.getDiySiteCode())).
                     collect(Collectors.toList());
             AppStore store = filterStoreList.get(0);
@@ -121,7 +123,10 @@ public class DataTransferServiceImpl implements DataTransferService {
         } else if ("送货上门".equals(tdOrder.getDeliverTypeTitle())) {
             List<TdDeliveryInfoDetails> tdDeliveryInfoDetailsList = this.queryDeliveryInfoDetailByOrderNumber(tdOrder.getMainOrderNumber());
             TdDeliveryInfoDetails tdDeliveryInfoDetails = tdDeliveryInfoDetailsList.get(0);
-            orderLogisticsInfo.setDetailedAddress(tdOrderLogistics.getDetailedAddress());
+            orderLogisticsInfo.setReceiver(tdOrderLogistics.getShippingName());
+            orderLogisticsInfo.setReceiverPhone(tdOrderLogistics.getShippingPhone());
+            orderLogisticsInfo.setShippingAddress(tdOrderLogistics.getShippingAddress());
+
             if (null == tdDeliveryInfoDetailsList || tdDeliveryInfoDetailsList.size() == 0) {
                 log.warn("物流信息没找到,订单：{}", tdOrder.getMainOrderNumber());
                 throw new DataTransferException("该订单没有找到物流信息", DataTransferExceptionType.DENF);
@@ -146,15 +151,12 @@ public class DataTransferServiceImpl implements DataTransferService {
         } else if ("郑州市".equals(tdOrder.getCity())) {
             orderLogisticsInfo.setDeliveryProvince("河南省");
         }
-
+        orderLogisticsInfo.setDetailedAddress(tdOrderLogistics.getDetailedAddress());
         orderLogisticsInfo.setDeliveryType(AppDeliveryType.getAppDeliveryTypeByDescription(tdOrder.getDeliverTypeTitle()));
         orderLogisticsInfo.setOrdNo(tdOrder.getMainOrderNumber());
         orderLogisticsInfo.setDeliveryCity(tdOrder.getCity());
         orderLogisticsInfo.setDeliveryCounty(tdOrderLogistics.getDisctrict());
         orderLogisticsInfo.setDeliveryStreet(tdOrderLogistics.getSubdistrict());
-        orderLogisticsInfo.setReceiver(tdOrderLogistics.getShippingName());
-        orderLogisticsInfo.setReceiverPhone(tdOrderLogistics.getShippingPhone());
-        orderLogisticsInfo.setShippingAddress(tdOrderLogistics.getShippingAddress());
         orderLogisticsInfo.setDeliveryTime(tdOrderLogistics.getDeliveryDate());
         orderLogisticsInfo.setIsOwnerReceiving(false);
         orderLogisticsInfo.setResidenceName(null);
