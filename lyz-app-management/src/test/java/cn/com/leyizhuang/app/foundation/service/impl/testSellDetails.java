e75f985a0fff47c46fc22ab7d25f1ddd417745bd
panjie <1742552645@qq.com>
2018-03-29 19:22:27 +0800

    订单商品转换修改

--- lyz-app-management/src/test/java/cn/com/leyizhuang/app/foundation/service/impl/testSellDetails.java
+++ lyz-app-management/src/test/java/cn/com/leyizhuang/app/foundation/service/impl/testSellDetails.java
@@ -1,6 +1,9 @@
 package cn.com.leyizhuang.app.foundation.service.impl;
 
+import cn.com.leyizhuang.app.core.constant.DataTransferExceptionType;
+import cn.com.leyizhuang.app.core.exception.DataTransferException;
 import cn.com.leyizhuang.app.foundation.dao.transferdao.TransferDAO;
+import cn.com.leyizhuang.app.foundation.pojo.datatransfer.TdOrder;
 import cn.com.leyizhuang.app.foundation.pojo.datatransfer.TdOrderGoods;
 import cn.com.leyizhuang.app.foundation.pojo.order.OrderBaseInfo;
 import cn.com.leyizhuang.app.foundation.service.datatransfer.OrderGoodsTransferService;
@@ -36,10 +39,17 @@ public class testSellDetails {
         //statisticsSellDetailsService.addOrderSellDetails("CD_XN20180319095037659959");
 
         List<OrderBaseInfo> orderBaseInfoList = transferDAO.findNewOrderNumber();
-        OrderBaseInfo baseInfo = appOrderService.getOrderByOrderNumber("CD_XN20171221124725763838");
+        OrderBaseInfo baseInfo = appOrderService.getOrderByOrderNumber("CD_XN20171001095945519243");
+
+        // 根据主单号 找到旧订单分单
+        List<TdOrder> tdOrders = transferDAO.findOrderAllFieldByOrderNumber(baseInfo.getOrderNumber());
+        if (tdOrders == null || tdOrders.size() == 0) {
+            //throw new Exception("订单商品转行异常，找不到旧订单 订单号："+ orderBaseInfo.getOrderNumber());
+            throw new DataTransferException("找不到旧订单 订单号："+ baseInfo.getOrderNumber(), DataTransferExceptionType.NDT);
+        }
         try {
             //orderGoodsTransferService.transferOne(orderBaseInfoList.get(0));
-            orderGoodsTransferService.transferOne(baseInfo);
+            orderGoodsTransferService.transferOne(baseInfo,tdOrders);
         } catch (Exception e) {
             e.printStackTrace();
 
