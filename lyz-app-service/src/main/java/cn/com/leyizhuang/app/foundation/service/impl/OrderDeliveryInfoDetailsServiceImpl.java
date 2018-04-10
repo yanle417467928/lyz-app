c270a86b46764162bc2f23378c4a5f91bba437d0
caiyu0608 <393655489@qq.com>
2018-04-10 22:52:30 +0800

    待配送列表

--- lyz-app-service/src/main/java/cn/com/leyizhuang/app/foundation/service/impl/OrderDeliveryInfoDetailsServiceImpl.java
+++ lyz-app-service/src/main/java/cn/com/leyizhuang/app/foundation/service/impl/OrderDeliveryInfoDetailsServiceImpl.java
@@ -17,6 +17,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -54,19 +55,34 @@ public class OrderDeliveryInfoDetailsServiceImpl implements OrderDeliveryInfoDet
 //        PageHelper.startPage(page, size);
         List<WaitDeliveryResponse> waitDeliveryResponseList = orderDeliveryInfoDetailsDAO.getOrderBeasInfoByOperatorNo(operatorNo);
         List<OrderArrearsAuditDO> orderArrearsAuditDOList = orderDeliveryInfoDetailsDAO.getArrearsAuditByOperatorNo(operatorNo);
-        List<WaitDeliveryResponse> waitDeliveryResponses = new ArrayList<>();
-        if (null != waitDeliveryResponseList && waitDeliveryResponseList.size() > 0) {
-            for(int i = 0;i<waitDeliveryResponseList.size();i++){
-                for (int j=0;j<orderArrearsAuditDOList.size();j++){
-                    /*2018-04-07 generation 报IndexOutOfBoundsException，原因是remove后，size变短*/
-                    if (!waitDeliveryResponseList.get(i).getOrderNumber().equals(orderArrearsAuditDOList.get(j).getOrderNumber())) {
-                        waitDeliveryResponses.add(waitDeliveryResponseList.get(i));
-
-                    }
+        Iterator<WaitDeliveryResponse> waitDeliveryResponseIterator = waitDeliveryResponseList.iterator();
+        Iterator<OrderArrearsAuditDO> orderArrearsAuditDOIterator = orderArrearsAuditDOList.iterator();
+        while (waitDeliveryResponseIterator.hasNext()){
+            while (orderArrearsAuditDOIterator.hasNext()){
+                WaitDeliveryResponse waitDeliveryResponse = waitDeliveryResponseIterator.next();
+                OrderArrearsAuditDO orderArrearsAuditDO = orderArrearsAuditDOIterator.next();
+                if (waitDeliveryResponse.getOrderNumber().equals(orderArrearsAuditDO.getOrderNumber())){
+                    waitDeliveryResponseIterator.remove();
                 }
             }
         }
-        return waitDeliveryResponses;
+
+//        List<WaitDeliveryResponse> waitDeliveryResponses = new ArrayList<>();
+//        if (null != waitDeliveryResponseList && waitDeliveryResponseList.size() > 0) {
+//            for(int i = 0;i<waitDeliveryResponseList.size();i++){
+//                for (int j=0;j<orderArrearsAuditDOList.size();j++){
+//                    /*2018-04-07 generation 报IndexOutOfBoundsException，原因是remove后，size变短*/
+//                    if (!waitDeliveryResponseList.get(i).getOrderNumber().equals(orderArrearsAuditDOList.get(j).getOrderNumber())) {
+//                        waitDeliveryResponses.add(waitDeliveryResponseList.get(i));
+//
+//                    }
+//                }
+//            }
+//        }
+
+
+//
+        return waitDeliveryResponseList;
     }
 
     @Override
