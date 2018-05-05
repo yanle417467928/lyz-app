aed4c0ed292793252fc5bd4e947cd4f4beebc792
liuhao <540654127@qq.com>
2018-05-05 16:54:23 +0800

    后台 销量报表导入

--- lyz-app-management/src/main/java/cn/com/leyizhuang/app/web/controller/rest/MaReportDownloadRestController.java
+++ lyz-app-management/src/main/java/cn/com/leyizhuang/app/web/controller/rest/MaReportDownloadRestController.java
@@ -1494,10 +1494,10 @@ public class MaReportDownloadRestController extends BaseRestController {
 
 
                 //列宽
-                int[] columnView = {10, 20, 10, 10, 30, 10, 10, 15, 15, 15, 15, 15, 20, 15, 15, 15, 15, 15, 15, 15, 15, 15};
+                int[] columnView = {10, 20, 10, 10, 30, 10, 10, 15, 15, 15, 15, 15, 20, 15, 15, 15, 15, 15, 15,15, 15, 15, 15};
                 //列标题城市
 
-                String[] titles = {"城市", "门店", "名称", "会员名称", "订单号", "配送/自提", "订单状态", "自提提货日期", "订单日期", "出货时间", "是否结清", "订单还清日期", "编号", "品牌", "财务销量", "经销财务销量", "经销单价", "原单价", "结算单价", "会员折扣", "折扣或者赠品分摊", "现金券", "下单数量"
+                String[] titles = {"城市", "门店", "名称", "会员名称", "订单号", "配送/自提", "订单状态", "自提提货日期", "订单日期", "出货时间", "是否结清", "订单还清日期", "编号","商品名称" ,"品牌", "财务销量", "经销财务销量", "经销单价", "原单价", "结算单价", "会员折扣", "折扣或者赠品分摊", "现金券", "下单数量"
                 };
                 //计算标题开始行号
                 int row = 1;
@@ -1528,9 +1528,10 @@ public class MaReportDownloadRestController extends BaseRestController {
                     ws.addCell(new Label(10, j + row, salesReportDO.getIsPayUp(), textFormat));
                     ws.addCell(new Label(11, j + row, salesReportDO.getPayUpTime(), textFormat));
                     ws.addCell(new Label(12, j + row, salesReportDO.getSku(), textFormat));
-                    ws.addCell(new Label(13, j + row, salesReportDO.getCompanyFlag(), textFormat));
-                    ws.addCell(new Label(14, j + row, salesReportDO.getFinancialSales(), textFormat));
-                    ws.addCell(new Label(15, j + row, salesReportDO.getDistributionSales(), textFormat));
+                    ws.addCell(new Label(13, j + row, salesReportDO.getSkuName(), textFormat));
+                    ws.addCell(new Label(14, j + row, salesReportDO.getCompanyFlag(), textFormat));
+                    ws.addCell(new Label(15, j + row, salesReportDO.getFinancialSales(), textFormat));
+                    ws.addCell(new Label(16, j + row, salesReportDO.getDistributionSales(), textFormat));
                     if (null != salesReportDO.getWholesalePrice()) {
                         ws.addCell(new Label(16, j + row, salesReportDO.getWholesalePrice().toString(), textFormat));
                     }
@@ -1656,10 +1657,10 @@ public class MaReportDownloadRestController extends BaseRestController {
 
 
                 //列宽
-                int[] columnView = {10, 20, 10, 10, 30, 10, 10, 15, 15, 15, 15, 15, 20, 15, 15, 15, 15, 15, 15, 15, 15, 15};
+                int[] columnView = {10, 20, 10, 10, 30, 10, 10, 15, 15, 15, 15, 15, 20, 15, 15, 15, 15, 15, 15, 15,15, 15, 15};
                 //列标题城市
 
-                String[] titles = {"城市", "门店", "名称", "会员名称", "订单号", "配送/自提", "订单状态", "自提提货日期", "订单日期", "出货时间", "是否结清", "订单还清日期", "编号", "品牌", "财务销量", "经销财务销量", "经销单价", "原单价", "结算单价", "会员折扣", "折扣或者赠品分摊", "现金券", "下单数量"
+                String[] titles = {"城市", "门店", "名称", "会员名称", "订单号", "配送/自提", "订单状态", "自提提货日期", "订单日期", "出货时间", "是否结清", "订单还清日期", "编号","商品名称", "品牌", "财务销量", "经销财务销量", "经销单价", "原单价", "结算单价", "会员折扣", "折扣或者赠品分摊", "现金券", "下单数量"
                 };
                 //计算标题开始行号
                 int row = 1;
@@ -1710,9 +1711,10 @@ public class MaReportDownloadRestController extends BaseRestController {
                     ws.addCell(new Label(10, j + row, salesReportDO.getIsPayUp(), textFormat));
                     ws.addCell(new Label(11, j + row, salesReportDO.getPayUpTime(), textFormat));
                     ws.addCell(new Label(12, j + row, salesReportDO.getSku(), textFormat));
-                    ws.addCell(new Label(13, j + row, salesReportDO.getCompanyFlag(), textFormat));
-                    ws.addCell(new Label(14, j + row, salesReportDO.getFinancialSales(), textFormat));
-                    ws.addCell(new Label(15, j + row, salesReportDO.getDistributionSales(), textFormat));
+                    ws.addCell(new Label(13, j + row, salesReportDO.getSkuName(), textFormat));
+                    ws.addCell(new Label(14, j + row, salesReportDO.getCompanyFlag(), textFormat));
+                    ws.addCell(new Label(15, j + row, salesReportDO.getFinancialSales(), textFormat));
+                    ws.addCell(new Label(16, j + row, salesReportDO.getDistributionSales(), textFormat));
                     if (null != salesReportDO.getWholesalePrice()) {
                         ws.addCell(new Label(16, j + row, salesReportDO.getWholesalePrice().toString(), textFormat));
                     }
