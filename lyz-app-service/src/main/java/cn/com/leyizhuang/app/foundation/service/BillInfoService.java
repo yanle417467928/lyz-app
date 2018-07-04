52b76bc429be7ee1ecf67dce6eb5f017c82c2ebb
panjie123 <pan123>
2018-07-04 16:29:28 +0800

    账单还款跳转支付页面

--- lyz-app-service/src/main/java/cn/com/leyizhuang/app/foundation/service/BillInfoService.java
+++ lyz-app-service/src/main/java/cn/com/leyizhuang/app/foundation/service/BillInfoService.java
@@ -5,6 +5,7 @@ import cn.com.leyizhuang.app.core.constant.OnlinePayType;
 import cn.com.leyizhuang.app.foundation.pojo.bill.BillInfoDO;
 import cn.com.leyizhuang.app.foundation.pojo.bill.BillRepaymentGoodsDetailsDO;
 import cn.com.leyizhuang.app.foundation.pojo.bill.BillRepaymentInfoDO;
+import cn.com.leyizhuang.app.foundation.pojo.request.BillorderDetailsRequest;
 import cn.com.leyizhuang.app.foundation.pojo.response.BillHistoryListResponse;
 import cn.com.leyizhuang.app.foundation.pojo.response.BillInfoResponse;
 import com.github.pagehelper.PageInfo;
@@ -56,5 +57,7 @@ public interface BillInfoService {
     void handleBillInfoInBillDate(Long storeId);
 
     BillInfoDO findBillInfoByBillEndDateAndStoreIdAndStatus(Long storeId, String billEndDate, BillStatusEnum status);
+
+    Double calculatePayAmount(Long storeId,List<BillorderDetailsRequest> orderDetails);
 
 }
